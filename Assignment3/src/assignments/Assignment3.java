be6f33758307ed68ce7fe4252ab66cdddad7df25
varunreddy <varunreddy6666@yahoo.com>
2016-06-03 20:14:50 +0530

    third assignment

---
+++ Assignment3/src/assignments/Assignment3.java
@@ -0,0 +1,17 @@
+package assignments;
+
+public class Assignment3 {
+
+	public static void main(String[] args) 
+	{
+		
+		int i=0; //can store intger values
+		
+	//	int f=10.01f;//can not store the float value
+		
+	//	int s="varun";//can not store String values
+		
+
+	}
+
+}
